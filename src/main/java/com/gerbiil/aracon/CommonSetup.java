cacaf1c0d045194e362da09320d6b928c79ec1a4
cursedgerbil <62775523+cursedgerbil@users.noreply.github.com>
2022-04-08 17:58:59 +0100

    Create CommonSetup.java

---
+++ src/main/java/com/gerbiil/aracon/CommonSetup.java
@@ -0,0 +1,10 @@
+package com.gerbiil.aracon.init;
+
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+
+public class CommonSetup {
+	public static void init(final FMLCommonSetupEvent event) {
+		
+	}
+
+}
